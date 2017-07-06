695841d286122944142babcb2481db620af449b4
argawinata94 <anggiarga.w@gmail.com>
2017-07-06 20:39:45 +0700

    Add files via upload

---
+++ interasi.cpp
@@ -0,0 +1,14 @@
+// iterasi_Rekursi
+#include <iostream>
+using namespace std;
+void numberFunction (int i) {
+	cout << "The number is: " << i << endl;
+}
+	int main () {
+	for(int i=0; i<10; i++) {
+	numberFunction(i);
+	}
+	return 0;
+	}
+
+	
