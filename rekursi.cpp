fc2663c0603d6ccf3514e721168c838fb9c6020a
argawinata94 <anggiarga.w@gmail.com>
2017-07-06 20:40:33 +0700

    Add files via upload

---
+++ rekursi.cpp
@@ -0,0 +1,16 @@
+//Rekursi
+#include <iostream> 
+using namespace std;
+
+void numberFunction(int i) {
+		cout << "The number is; " << i << endl;
+		i++; //Penghenti
+		if(i<10) {
+		numberFunction(i);
+		}
+	}
+int main () {
+	int i = 0;
+	numberFunction(i);
+	return 0;
+}
